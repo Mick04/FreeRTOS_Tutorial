ec4d9f5e004a99399ebf82ced3dfa07038a4a045
Michael Cave <michaelcave@Michaels-Mac-mini-Pro.local>
2025-12-31 10:46:43 +0000

    Added code to simulate WiFi drop out

--- src/WiFiService.cpp
+++ src/WiFiService.cpp
@@ -4,48 +4,61 @@
 static WiFiState wifiState = WIFI_CONNECTING;
 static TaskHandle_t wifiTaskHandle = nullptr;
 
-static void WiFiTask(void* pvParameters) {
+static void WiFiTask(void *pvParameters)
+{
     WiFi.begin("Gimp", "FC7KUNPX");
     wifiState = WIFI_CONNECTING;
 
-    for (;;) {
-             // ---- TEST HOOK (safe to remove later) ----
-        if (Serial.available()) {
+    for (;;)
+    {
+
+        //==============================================
+        // ---- TEST HOOK (safe to remove later) ----
+        //==============================================
+        if (Serial.available())
+        {
             char c = Serial.read();
-            if (c == 'd') {
+            if (c == 'd')
+            {
                 Serial.println("Forcing WiFi disconnect (test)");
                 WiFi.disconnect(true);
                 wifiState = WIFI_DISCONNECTED;
                 vTaskDelay(3000);
             }
         }
-        // ------------------------------------------
-        if (WiFi.status() == WL_CONNECTED) {
+        //==============================================
+        // ---- TEST HOOK (safe to remove later) ----
+        //==============================================
+
+        if (WiFi.status() == WL_CONNECTED)
+        {
             wifiState = WIFI_CONNECTED;
             Serial.println("WiFi Connected");
-        } else {
+        }
+        else
+        {
             wifiState = WIFI_CONNECTING;
             Serial.println("WiFi Disconnected, attempting to reconnect...");
             WiFi.disconnect();
             WiFi.begin("Gimp", "FC7KUNPX");
         }
-      
+
         vTaskDelay(pdMS_TO_TICKS(5000)); // Check every 5 seconds
     }
 }
-    void WiFiService::inti() {
-            xTaskCreatePinnedToCore(
-                WiFiTask,
-                "WiFiTask",
-                4096,
-                nullptr,
-                2,
-                &wifiTaskHandle,
-                0
-            );
-        }
+void WiFiService::inti()
+{
+    xTaskCreatePinnedToCore(
+        WiFiTask,
+        "WiFiTask",
+        4096,
+        nullptr,
+        2,
+        &wifiTaskHandle,
+        0);
+}
 
-WiFiState WiFiService::getState() {
+WiFiState WiFiService::getState()
+{
     return wifiState;
-}   
-        
+}
